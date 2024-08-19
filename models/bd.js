763272c26c07071048b3ef455fe848473ea09095
Daniel Osvaldo Duda <daniel_duda93@yahoo.com.ar>
2024-08-19 17:49:57 -0300

    Update bd.js

--- models/bd.js
+++ models/bd.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql2/promise');
+var mysql = require('mysql2');
 var util = require('util');
 var dotenv = require('dotenv');
 
@@ -6,17 +6,16 @@ var dotenv = require('dotenv');
 dotenv.config();
 
 var pool = mysql.createPool({
-
-connectionLimit: 10,
-port: "3306",
-host: process.env.MYSQLDB_HOST,
-user: process.env.MYSQLDB_USER,
-password: process.env.MYSQLDB_PASSWORD,
-database: process.env.MYSQLDB_DATABASE,
-
-
+  connectionLimit: 10,
+  port: "3307",
+  host: process.env.MYSQLDB_HOST,
+  user: process.env.MYSQLDB_USER,
+  password: process.env.MYSQLDB_PASSWORD,
+  database: process.env.MYSQLDB_DATABASE,
 });
 
+// Prometificar el método query
+pool.query = util.promisify(pool.query);
 
 // Imprimir un mensaje en la consola cuando la conexión esté establecida
 pool.on('connection', () => {
